2dcad4ce3465fb65be3a486c9beff599130fb901
João Vitor <joao-vitor.dias@lexartlabs.com>
2021-06-08 11:48:25 -0300

    feat: add switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, Switch } from 'react-native';
 
 export function Header() {
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>to.</Text>
       <Text style={[styles.headerText, { fontFamily: 'Poppins-SemiBold' }]}>do</Text>
+      <Switch
+      // trackColor={{ false: "#767577", true: "#81b0ff" }}
+      // thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+      // ios_backgroundColor="#3e3e3e"
+      // onValueChange={toggleSwitch}
+      // value={isEnabled}
+      />
+
     </View>
   )
 }
